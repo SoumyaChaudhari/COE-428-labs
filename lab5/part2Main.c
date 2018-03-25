9ede3b71f5b679c55e3746ca780298541db9350e
SoumyaChaudhari <s28chaud@ryerson.ca>
2018-03-25 18:16:39 -0400

    Officially final changes to part1Main & intStack

--- lab5/part2Main.c
+++ lab5/part2Main.c
@@ -3,46 +3,17 @@
 #include <ctype.h>
 extern int pop();
 extern void push(int);
-extern int isEmpty();
 
 int main(int argc, char * argv[])
 {
-  int ch, poppedch; // ch is from stdin and poppedch is the c
-  int counterarr[25] = '0';
+  int ch;
   while ((ch = getchar()) != EOF) {
-    if (!(isalpha(ch) || ch == '>' || ch == '<' || ch == '/')) //????
+    if (!(isalpha(ch) || ch == '>' || ch == '<' || ch == '/'))
       continue;
-    if(ch=='<'){
-        ch = getchar(); //Take another stdin
-        if(isalpha(ch)){ // If ch is an alphabet then push it onto the stack
-            push(ch);
-        }
-        else if(ch =='/'){
-            ch = getchar();
-            poppedch = pop();
-            //Let's check if the stack is empty.
-            if((isEmpty()) == 1)
-               exit(0);
-            else if(poppedch != ch )
-            {
-                printf("Invalid!\n");
-                exit(1);
-            }
-        }
-    }
-
+    //
+    
+    
+    
   }
-  if(isEmpty()== 0){
-    fprintf(stderr,"Invalid!\n");
-    exit(1);}
-  else{
-    fprintf(stderr,"Valid!\n");
-    for(int i = 0: 25){
-        if(counterarr[i] == '0'){
-            printf("%c %d\n", (i + 'a'));
-        }
-
-     }
-     exit(0);
-   }
+  exit(0);
 }
